267b2372e854c834b45e5fbf06cda95debd100bb
Lívia <livianamiesn@gmail.com>
2021-06-22 15:45:59 -0300

    feat: added the structure of the search feature

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,33 @@
-import { Container, PageInfo, Logo, SearchFeature, MoreIcon, SearchIcon} from './styles';
+import { useEffect } from 'react';
+import { useCallback, useState } from 'react';
+import { IUser } from '../../models';
+import api from '../../services/api';
+import { Container, PageInfo, Logo, SearchFeature, MoreIcon, PopUpWrapper, PopUp, CloseIcon, UsersList, User, Names} from './styles';
 
 const Header: React.FC = () => {
+  const [popUpDisplay, setPopUpDisplay] = useState('none');
+  const [users, setUsers] = useState([]);
+  
+
+  useEffect(() => {
+    const loadUsers = async () => {
+      const response = await api.get('/users')
+      console.log(response.data)
+      setUsers(response.data);
+    }
+    loadUsers()
+  }, []);
+  
+
+  const openPopUp = useCallback (() => {
+    setPopUpDisplay('flex')
+  }, [])
+
+  const closePopUp = useCallback (() => {
+    setPopUpDisplay('none')
+  }, [])
+
+
   return (
     <Container>
       <PageInfo>
@@ -11,9 +38,30 @@ const Header: React.FC = () => {
       <button id="more" type="submit"><MoreIcon /></button>
 
       <SearchFeature>
-        <input type="text" placeholder='Buscar' />
-        <button type="submit"><SearchIcon /></button>
+        <button onClick={openPopUp}>Buscar</button>
       </SearchFeature>
+
+      <PopUpWrapper style={{display: `${popUpDisplay}`}}>
+        <PopUp >
+          <input type="text" placeholder='Buscar' />
+        
+          <UsersList>
+            {users.map((user: IUser) => {
+              return (
+                <User>
+                  <img src={user.photo} alt="" />
+                  <Names>
+                    <strong>{user.first_name} {user.last_name}</strong>
+                    <span>@{user.username}</span>
+                  </Names>
+                </User>
+              )
+            })}
+          </UsersList>
+
+          <button onClick={closePopUp}><CloseIcon /></button>
+        </PopUp>
+      </PopUpWrapper>
     </Container>
   );
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { List, Search, Earlybirds } from '../../styles/Icons';
+import { List, Search, Earlybirds, XCircle } from '../../styles/Icons';
 
 export const Container = styled.div`
   z-index: 2;
@@ -49,6 +49,187 @@ export const PageInfo = styled.div`
     }
   }
 `;
+
+export const SearchFeature = styled.div`
+  display: none;
+  align-items: center;
+
+  @media (min-width: 800px) {
+    display: flex;
+  }
+
+  > button {
+    width: 13rem;
+    height: 2.5rem;
+
+    padding: 0.1rem 0.5rem;
+
+    border: 0.15rem solid #7C00F3;
+    border-radius: 0.8rem;
+    box-shadow: 0.2rem 0.2rem 0 0 #FFF500;
+
+    cursor: pointer;
+
+    background-color: var(--purple);
+
+    color: var(--yellow);
+    text-shadow: 0.1rem 0.1rem 0 #000000;
+    font-size: 1.3rem;
+
+    &:hover,
+    &:active {
+      color: var(--darker-purple);
+    }
+
+    @media (min-width:1200px) {
+      width: 14rem;
+      height: 3rem;
+
+      padding: 0.1rem 1rem;
+
+      font-size: 1.4rem;
+    }
+  }
+`;
+
+export const PopUpWrapper = styled.div`
+  display: none;
+  align-items: center;
+  justify-content: center;
+
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  z-index: 1;
+
+  background-color: rgba(0, 0, 0, 0.25);
+
+  width: 100vw;
+  height: 100%;
+`;
+
+export const PopUp = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+
+  padding: 3rem;
+
+  background-color: white;
+
+  border-radius: 0.8rem;
+  border: 0.15rem solid #7C00F3;
+
+  > input {
+    width: 15rem;
+    height: 2.5rem; 
+
+    margin-bottom: 2rem;
+    padding: 0.3rem 0.6rem;
+
+    background-color: var(--lighter-purple);
+
+    border: 0.15rem solid var(--darker-purple);
+    border-radius: 0.8rem;
+    box-shadow: 0.2rem 0.2rem 0 0 var(--yellow);
+
+    font-size: 1.1rem;
+
+    @media (min-width:1200px) {
+      width: 18rem;
+      height: 3rem;
+
+      font-size: 1.3rem;
+    }
+  }
+`;
+
+export const UsersList = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  width: 100%;
+  height: 30rem;
+
+  border: 0.15rem solid var(--darker-purple);
+  border-radius: 0.8rem;
+
+  overflow-y: scroll;
+
+  ::-webkit-scrollbar {
+    width: 10px;
+  }
+  ::-webkit-scrollbar-track {
+    background: #f1f1f1;
+    border-radius: 0.8rem;
+  }
+  ::-webkit-scrollbar-thumb {
+    background: #888;
+    border-radius: 0.8rem;
+  }
+  ::-webkit-scrollbar-thumb:hover {
+    background: #555;
+  } 
+`;
+
+export const User = styled.div`
+  display: flex;
+  align-items: center;
+
+  width: 25rem;
+
+  padding: 1rem 2rem;
+
+  cursor: pointer;
+
+  border-top: 0.05rem solid var(--purple);
+
+  :first-child {
+    border-top: none;
+  }
+
+  > img {
+    width: 3.8rem;
+    height: 3.8rem;
+
+    margin-right: 1rem;
+
+    object-fit: cover;
+
+    border: 0.15rem solid var(--darker-purple);
+    border-radius: 50%;
+
+    cursor: pointer;
+
+    @media (min-width: 1200px) {
+      width: 4rem;
+      height: 4rem;
+    }
+  }
+`;
+
+export const Names = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+
+  > strong {
+    font-size: 1.1rem;
+    
+    @media (min-width:1200px) {
+      font-size: 1.3rem;
+    }
+  }
+  > p {
+    font-size: 1.1rem;
+    
+    @media (min-width:1200px) {
+      font-size: 1.2rem;
+    }
+  };
+`;
+
 export const Logo = styled(Earlybirds)`
   width: 2rem;
   color: var(--purple);
@@ -67,61 +248,6 @@ export const Logo = styled(Earlybirds)`
     margin-right: 2rem;
   }
 `;
-export const SearchFeature = styled.div`
-  display: none;
-  align-items: center;
-
-  @media (min-width: 800px) {
-    display: flex;
-  }
-
-  > input {
-    width: 10rem;
-    height: 2.5rem; 
-
-    padding: 0.3rem 0.6rem;
-
-    border: 0.15rem solid #7C00F3;
-    border-radius: 0.8rem 0 0 0.8rem;
-    box-shadow: 0 0.2rem 0 0 #FFF500;
-
-    font-size: 1rem;
-
-    @media (min-width: 800px) {
-      width: 11rem;
-    }
-    @media (min-width:1200px) {
-      width: 14rem;
-      height: 3rem;
-
-      font-size: 1.2rem;
-    }
-  }
-
-  > button {
-    height: 2.5rem;
-    padding: 0.1rem 0.5rem;
-
-    border: 0.15rem solid #7C00F3;
-    border-left: 0;
-    border-radius: 0 0.8rem 0.8rem 0;
-    box-shadow: 0.2rem 0.2rem 0 0 #FFF500;
-
-    @media (min-width:1200px) {
-      height: 3rem;
-      padding: 0.1rem 0.8rem;
-    }
-  }
-`;
-export const SearchIcon = styled(Search)`
-  width: 1.1rem;
-
-  cursor: pointer;
-
-  @media (min-width:1200px) {
-    width: 1.5rem;
-  }
-`;
 export const MoreIcon = styled(List)`
   width: 1.6rem;
 
@@ -131,4 +257,17 @@ export const MoreIcon = styled(List)`
     display: none;
   }
 `;
+export const CloseIcon = styled(XCircle)`
+  width: 2rem;
+  color: var(--darker-purple);
+
+  margin-top: 3rem;
+
+  cursor: pointer;
+
+  @media (min-width: 1200px) {
+    width: 3rem
+  }
+`;
+
 
